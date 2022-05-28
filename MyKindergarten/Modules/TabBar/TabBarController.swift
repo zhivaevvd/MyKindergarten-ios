8cb9cf26aeb92d104f8016f2e51fe441a290aaa6
zhivaeevvd <mass2014@yandex.ru>
2022-05-29 00:57:14 +0300

    remove chat

--- MyKindergarten/Modules/TabBar/TabBarController.swift
+++ MyKindergarten/Modules/TabBar/TabBarController.swift
@@ -18,20 +18,20 @@ public class TabBarController: UITabBarController {
 
     private var profileVC: UIViewController = UINavigationController(rootViewController: VCFactory.buildProfileVC())
 
-    private var chatVC: UIViewController = UINavigationController(rootViewController: VCFactory.buildChatVC())
+    //private var chatVC: UIViewController = UINavigationController(rootViewController: VCFactory.buildChatVC())
 
     private var scheduleVC: UIViewController = UINavigationController(rootViewController: VCFactory.buildScheduleVC())
 
     private func setupTabBar() {
         modalPresentationStyle = .fullScreen
-        setViewControllers([scheduleVC, profileVC, chatVC], animated: false)
+        setViewControllers([scheduleVC, profileVC], animated: false)
         scheduleVC.title = L10n.Tabbar.schedule
         scheduleVC.tabBarItem.image = UIImage(systemName: "calendar")
 
         profileVC.title = L10n.Tabbar.profile
         profileVC.tabBarItem.image = UIImage(systemName: "person.fill")
 
-        chatVC.title = "Чат"
-        chatVC.tabBarItem.image = UIImage(systemName: "message.fill")
+//        chatVC.title = "Чат"
+//        chatVC.tabBarItem.image = UIImage(systemName: "message.fill")
     }
 }
